29521d285eca9da32baf6511188a72f57dfe8d1d
Abdul <malik301002@gmail.com>
2024-08-05 23:39:26 +0700

    chore: fonts setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        clash: ['ClashDisplay', 'sans-serif'],
+        caveat: ['Caveat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
