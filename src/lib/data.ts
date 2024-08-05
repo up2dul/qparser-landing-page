16c3959fddfcd898f3ad75b53591191e771e43ff
Abdul <malik301002@gmail.com>
2024-08-06 02:55:38 +0700

    feat(data): add advantages and pricings

---
+++ src/lib/data.ts
@@ -0,0 +1,68 @@
+export const navs = [
+  {
+    title: 'About',
+    href: '/about',
+  },
+  {
+    title: 'How it work',
+    href: '/how-it-work',
+  },
+  {
+    title: 'Pricing',
+    href: '/pricing',
+  },
+  {
+    title: 'Solution',
+    href: '/solution',
+  },
+  {
+    title: 'Features',
+    href: '/features',
+  },
+];
+
+export const advantages = [
+  'Fully proxyless google parsing',
+  'Unlock unlimited search query',
+  'Fast and Efficient',
+  'Access full customers support',
+];
+
+export const pricings = [
+  {
+    title: 'Weekly',
+    description: 'Special first packet for all',
+    price: '$10.99',
+    period: 'Week',
+    features: [
+      'Up to 10 pages to scraped',
+      'Unlimited queries',
+      'Access all feature inside',
+    ],
+  },
+  {
+    title: '1 Month',
+    description: 'Recommended for professionals',
+    price: '$24.99',
+    period: 'Month',
+    features: [
+      'Up to 10 pages to scraped',
+      'Unlimited queries',
+      'Access all feature inside',
+      'Access full customers support',
+    ],
+  },
+  {
+    title: '3 Month',
+    description: 'Recommended for experts',
+    price: '$22.50',
+    period: 'Month',
+    features: [
+      'Up to 10 pages to scraped',
+      'Unlimited queries',
+      'Access all feature inside',
+      'Access full customers support',
+      'More cheapest than 1 month',
+    ],
+  },
+];
