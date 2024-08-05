d16bb89e4cc02734f63397a4b5b8ebc0cc28cdc0
Abdul <malik301002@gmail.com>
2024-08-06 02:58:51 +0700

    chore(tailwind): replace colors and extend screens by adding `xs`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,23 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#FFF',
+      green: '#A8FF35',
+      'dark-50': '#585858',
+      'dark-100': '#323232',
+      'dark-200': '#2E2E2E',
+      'dark-300': '#181818',
+      'dark-400': '#050505',
+    },
     extend: {
       fontFamily: {
         clash: ['ClashDisplay', 'sans-serif'],
         caveat: ['Caveat', 'sans-serif'],
+      },
+      screens: {
+        xs: '480px',
       },
     },
   },
