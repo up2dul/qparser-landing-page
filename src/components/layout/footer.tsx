84185207a3e3833509bf5ac4701d23f4808294c3
Abdul <malik301002@gmail.com>
2024-08-07 00:44:01 +0700

    feat(comps): responsive footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,10 +3,10 @@ import { navs } from '~/lib/data';
 
 export const Footer = () => {
   return (
-    <footer className="py-6 flex justify-between items-center">
+    <footer className="py-6 px-[80px] xl:px-[150px] flex flex-col justify-between items-center gap-16 lg:flex-row">
       <NavLogo />
 
-      <ul className="flex justify-between items-center gap-[57px]">
+      <ul className="flex flex-col justify-between items-center gap-[57px] lg:flex-row">
         {navs.map(nav => (
           <li key={nav.title}>
             <Link href={nav.href}>{nav.title}</Link>
