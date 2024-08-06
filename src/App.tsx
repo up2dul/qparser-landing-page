23ad0e3c70e5fea351bb790d712e0b3a2cc08bc3
Abdul <malik301002@gmail.com>
2024-08-06 07:36:35 +0700

    feat: show some section components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Header } from './components/layout';
+import { Detail, Hero, Install, Payments } from './components/section';
 
 function App() {
   return (
     <>
       <Header />
+
+      <main className="mt-20">
+        <Hero />
+        <Install />
+        <Detail />
+        <Payments />
+      </main>
     </>
   );
 }
