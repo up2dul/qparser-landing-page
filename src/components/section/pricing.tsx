d7ec4e753e2e8516492840430eca91831ee89b0f
Abdul <malik301002@gmail.com>
2024-08-08 03:50:37 +0700

    feat: add radix switch to pricing section
    Along with the styling in index.css

--- src/components/section/pricing.tsx
+++ src/components/section/pricing.tsx
@@ -1,3 +1,5 @@
+import * as Switch from '@radix-ui/react-switch';
+
 import { PricingCard } from '~/components/common';
 import { pricings } from '~/lib/data';
 
@@ -7,6 +9,16 @@ export const Pricing = () => {
       <h1 className="font-bold text-grey text-3xl md:text-5xl xl:text-6xl">
         Get Your Best Deal
       </h1>
+
+      <div className="mt-[43px] md:mt-[50px] flex justify-center items-center gap-8 text-white">
+        <h2 className="font-medium">Monthly</h2>
+
+        <Switch.Root className="switch-root">
+          <Switch.Thumb className="switch-thumb" />
+        </Switch.Root>
+
+        <h2>Yearly</h2>
+      </div>
 
       <ul className="mt-12 flex flex-wrap justify-center items-center gap-10">
         {pricings.map(pricing => (
